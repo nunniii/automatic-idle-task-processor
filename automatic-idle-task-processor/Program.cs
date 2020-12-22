8f00de1a0d4b25f1f4e74280abefe0b43ae78f69
Mateus Nunes <mateusnss@protonmail.ch>
2020-12-22 15:28:41 -0300

    init

---
+++ automatic-idle-task-processor/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+
+namespace automatic_idle_task_processor
+{
+    class Program
+    {
+        static void RunRundll()
+        {
+            Process.Start("..\\..\\rundll32.bat");
+        }
+
+        static void Main(string[] args)
+        {
+            while(true)
+            {
+                RunRundll();
+                Thread.Sleep(TimeSpan.FromMinutes(0.5));
+            }
+        }
+    }
+}
---
+++ automatic-idle-task-processor/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("automatic-idle-task-processor")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("automatic-idle-task-processor")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("543dd8b5-1cdb-404d-820b-64a0b58fdc2b")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+// É possível especificar todos os valores ou usar como padrão os Números de Build e da Revisão
+// usando o "*" como mostrado abaixo:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
